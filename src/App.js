06e261b6854500c4c8b4a16a0b4ec67f2671d1ff
shinbum.kang <tlsqja1230@naver.com>
2020-05-17 02:40:23 +0900

    propTypes 모듈 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'
+
+function Food({name, picture, rating}){
+  return (
+    <div>
+      <h1>I love {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
+    </div>
+  )
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+}
+
+const foodArray = [
+  {
+    id:"1",
+    name: "kimch",
+    picture: "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile4.uf.tistory.com%2Fimage%2F2164B93656404366065143",
+    rating: 4.8
+  },
+  {
+    id:"2",
+    name: "kimbab",
+    picture: "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile4.uf.tistory.com%2Fimage%2F2164B93656404366065143",
+    rating: 3.5
+  },
+  {
+    id:"3",
+    name: "cheese",
+    picture: "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile4.uf.tistory.com%2Fimage%2F2164B93656404366065143",
+    rating: 4.3
+  },
+  {
+    id:"4",
+    name: "spam",
+    picture: "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile4.uf.tistory.com%2Fimage%2F2164B93656404366065143",
+    rating: 2.4
+  },
+  {
+    id:"5",
+    name: "egg",
+    picture: "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile4.uf.tistory.com%2Fimage%2F2164B93656404366065143",
+    rating: 3.4
+  },
+]
+
+function renderFood (item) {
+  return <Food key={item.id} name={item.name} picture={item.picture} rating={item.rating}/>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodArray.map(renderFood)}
     </div>
   );
 }
